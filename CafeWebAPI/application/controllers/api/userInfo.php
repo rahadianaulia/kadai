46ece6ac46c50ee9a9083d1e90b4899e66fd5b3a
msskylab <msskylab@gmail.com>
2015-09-14 23:24:27 +0700

    commit

--- CafeWebAPI/application/controllers/api/userInfo.php
+++ CafeWebAPI/application/controllers/api/userInfo.php
@@ -25,7 +25,16 @@ class UserInfo extends REST_Controller
         }
 
     }
+    function listUser_get()
+    {
+        $data = $this->user_model->getUsers();
+        if($data){
+            $this->response($data,200);
+        }else{
+            $this->response(null,404);
+        }
 
+    }
     function register_post()
     {
         // $username = $this->post('username');
