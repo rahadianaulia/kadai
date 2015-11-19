7c66801e37bafc335f1752f3688c8f08d570a2e0
rahadian <rahadian.af@gmail.com>
2015-11-19 10:36:34 +0700

    fix login

--- app/loginCtrl.js
+++ app/loginCtrl.js
@@ -1,10 +1,10 @@
 (function(){
     angular.module('mainApp')
-    .controller('loginCtrl', function($scope, $http, baseUrl, $modal, toaster){
+    .controller('loginCtrl', function($scope, $cookies){
         $scope.login = function(){
         	var username = $scope.username;
         	var password = $scope.pass;
-        	
+        	$cookies.putObject("userInfo", username+password);
         };
     })
 }());
